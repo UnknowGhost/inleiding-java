d9df2f1089d31f5f40c1ee198b065021af7e50e1
Justin <justin-z@live.nl>
2017-08-30 13:12:59 +0200

    lolzz

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -5,7 +5,5 @@ package h01;
  */
 public class HalloRocDev {
 
-    public static void main(String[] args) {
-        System.out.println("Hallo roc-dev!");
-    }
-}
+    public static void main(String[] args) { System.out.println(lolzz); }
+    
